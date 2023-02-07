3632199c5fe50dddf1db3fbe1be22286f6b99c15
BengieMine25 <est.bmedinao0201@uml.edu.ni>
2023-02-07 11:31:21 -0600

    trimestres

---
+++ src/modelos/trimestres.js
@@ -0,0 +1,21 @@
+//Importar la configuracion
+import { app } from '../js/firebase'
+
+// Importar los modulos de firebase necesarios
+import {
+    getFirestore, 
+    collection,
+    getDocs, 
+    onSnapshot,
+    addDoc,
+    doc,
+    getDoc,
+    updateDoc } from 'firebase/firestore' 
+
+    //Conectarnos a la base de datos
+export const db = getFirestore();
+
+//Guardar un trimestre
+export const guardarTrimestre = (datos) => {
+    addDoc(collection(db, 'trimestres'), datos)
+}
